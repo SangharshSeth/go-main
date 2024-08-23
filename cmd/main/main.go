7914189eb2ff7946e86ecf51e6324fa3c2c3fd75
Sangharsh Seth <sethsangharsh@outlook.com>
2024-08-23 12:52:23 +0530

    Added healthcheck route on /

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"github.com/sangharshseth/internal/handlers"
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/sangharshseth/internal/handlers"
 )
 
 // func init() {
@@ -33,6 +35,10 @@ func main() {
 	mux := http.NewServeMux()
 	// Set up routes
 	handlers.SetupVideoRoutes(mux)
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "HealthCheck OK")
+	})
 
 	// Start the server
 	log.Println("Starting server on :8080")
