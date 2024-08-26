6e9b97942bba82928cabe5e7c401a98a2d89a0a6
Sangharsh Seth <sethsangharsh@outlook.com>
2024-08-26 17:16:34 +0530

    Added github actions

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -5,20 +5,21 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/joho/godotenv"
 	"github.com/sangharshseth/internal/handlers"
 )
 
-// func init() {
-// 	if err := godotenv.Load(); err != nil {
-// 		log.Fatal("Error Loading .env file")
-// 	}
-// }
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error Loading .env file")
+	}
+}
 func withCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Set CORS headers
-		w.Header().Set("Access-Control-Allow-Origin", "*") // Allow all origins
+		w.Header().Set("Access-Control-Allow-Origin", "*")                   // Allow all origins
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS") // Allow methods
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type") // Allow headers
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")       // Allow headers
 
 		// Handle preflight requests
 		if r.Method == http.MethodOptions {
@@ -35,7 +36,7 @@ func main() {
 	mux := http.NewServeMux()
 	// Set up routes
 	handlers.SetupVideoRoutes(mux)
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintf(w, "HealthCheck OK")
 	})
